7ca87cc7b57e2963fc697fd620fd837d265ffb81
Efim Belyakov <belyakov.efim@gmail.com>
2021-05-27 09:02:10 +0300

    Ffx Lab8

--- lab8/z1.py
+++ lab8/z1.py
@@ -79,7 +79,7 @@ def z2_5DeleteById(tdict: dict, id: int):
 
 def z2_6LimitBy22(tdict: dict):
     for i in tdict.keys():
-        if tdict.get(i)[1] == 22:
+        if tdict.get(i)[1] > 22:
             tdict.get(i)[1] -= 1
 
     return tdict
@@ -96,7 +96,7 @@ def z2_7DeleteBy23(tdict: dict):
 
 def z2_8AddAgeByIvanov(tdict: dict):
     for i in tdict.keys():
-        if re.match("^[а-я]* ", tdict.get(i)[0], flags=re.IGNORECASE):
+        if re.match("^Иванов ", tdict.get(i)[0], flags=re.IGNORECASE):
             tdict.get(i)[1] += 1
 
     return tdict
@@ -104,7 +104,7 @@ def z2_8AddAgeByIvanov(tdict: dict):
 
 def z2_9ReplaceLNToSidorov(tdict: dict):
     for i in tdict.keys():
-        tdict.get(i)[0] = re.sub("^[а-я]* ", 'Сидоров ', tdict.get(i)[0], flags=re.IGNORECASE)
+        tdict.get(i)[0] = re.sub("^Иванов ", 'Сидоров ', tdict.get(i)[0], flags=re.IGNORECASE)
 
     return tdict
 
@@ -207,14 +207,17 @@ s = ttk.Style()
 s.theme_use('classic')
 
 tableFrame = ttk.Frame(root)
-tableFrame.grid(column=0, row=0, sticky=tk.EW, ipadx=10, ipady=10)
-tableFrame.columnconfigure(0, weight=1)
-tableFrame.columnconfigure(1, weight=1)
-tableFrame.columnconfigure(2, weight=1)
-tableFrame.columnconfigure(3, weight=1)
-
 
 def drawTable(dtd):
+    global tableFrame
+    tableFrame.grid_forget()
+    tableFrame = ttk.Frame(root)
+    tableFrame.grid(column=0, row=0, sticky=tk.EW, ipadx=10, ipady=10)
+    tableFrame.columnconfigure(0, weight=1)
+    tableFrame.columnconfigure(1, weight=1)
+    tableFrame.columnconfigure(2, weight=1)
+    tableFrame.columnconfigure(3, weight=1)
+
     ttk.Label(tableFrame, text='№', width=5).grid(column=0, row=0, sticky=tk.NW, pady=10)
     ttk.Label(tableFrame, text='ФИО', width=30).grid(column=1, row=0, sticky=tk.NW, pady=10)
     ttk.Label(tableFrame, text='Возраст', width=30).grid(column=2, row=0, sticky=tk.NW, pady=10)
@@ -252,13 +255,17 @@ z22toFIO = ttk.Entry(z2_2)
 z22fromFIO.grid(column=0, row=0)
 z22toFIO.grid(column=1, row=0)
 
-ttk.Button(z2_2, text="Изменить ФИО", command=lambda: callFunc(z2_2ReplaceByFIO, [data_dict, z22fromFIO.get(), z22toFIO.get()])).grid(column=2, row=0, sticky=tk.NW)
+ttk.Button(z2_2, text="Изменить ФИО",
+           command=lambda: callFunc(z2_2ReplaceByFIO, [data_dict, z22fromFIO.get(), z22toFIO.get()])).grid(column=2,
+                                                                                                           row=0,
+                                                                                                           sticky=tk.NW)
 z2_2.grid(column=0, row=1)
 
 z2_3 = ttk.Frame(buttonFrame)
 z23Numb = ttk.Entry(z2_3)
 z23Numb.grid(column=0, row=0)
-ttk.Button(z2_3, text="Увеличить возраст", command=lambda: callFunc(z2_3FindById, [data_dict, int(z23Numb.get())])).grid(column=1, row=0, sticky=tk.NW)
+ttk.Button(z2_3, text="Увеличить возраст",
+           command=lambda: callFunc(z2_3FindById, [data_dict, int(z23Numb.get())])).grid(column=1, row=0, sticky=tk.NW)
 z2_3.grid(column=0, row=2)
 
 z2_4 = ttk.Frame(buttonFrame)
@@ -266,19 +273,28 @@ z24FIO = ttk.Entry(z2_4)
 z24FIO.grid(column=0, row=0)
 z24Num = ttk.Entry(z2_4)
 z24Num.grid(column=1, row=0)
-ttk.Button(z2_4, text="Изменить группу", command=lambda: callFunc(z2_4ReplaceGroupByFIO, [data_dict, z24FIO.get(), z24Num.get()])).grid(column=2, row=0, sticky=tk.NW)
+ttk.Button(z2_4, text="Изменить группу",
+           command=lambda: callFunc(z2_4ReplaceGroupByFIO, [data_dict, z24FIO.get(), z24Num.get()])).grid(column=2,
+                                                                                                          row=0,
+                                                                                                          sticky=tk.NW)
 z2_4.grid(column=0, row=3)
 
 z2_5 = ttk.Frame(buttonFrame)
 z25Num = ttk.Entry(z2_5)
 z25Num.grid(column=0, row=0)
-ttk.Button(z2_5, text="Удалить студента", command=lambda: callFunc(z2_5DeleteById, [data_dict, int(z25Num.get())])).grid(column=1, row=0, sticky=tk.NW)
+ttk.Button(z2_5, text="Удалить студента",
+           command=lambda: callFunc(z2_5DeleteById, [data_dict, int(z25Num.get())])).grid(column=1, row=0, sticky=tk.NW)
 z2_5.grid(column=0, row=4)
 
-ttk.Button(buttonFrame, text="Уменьшить возраст (22)", command=lambda: callFunc(z2_6LimitBy22, [data_dict])).grid(column=0, row=5, sticky=tk.NW)
-ttk.Button(buttonFrame, text="Удалить студента (23)", command=lambda: callFunc(z2_7DeleteBy23, [data_dict])).grid(column=0, row=6, sticky=tk.NW)
-ttk.Button(buttonFrame, text="Увеличить возраст у Иванов", command=lambda: callFunc(z2_8AddAgeByIvanov, [data_dict])).grid(column=0, row=7, sticky=tk.NW)
-ttk.Button(buttonFrame, text="Иванов на Сидоров", command=lambda: callFunc(z2_9ReplaceLNToSidorov, [data_dict])).grid(column=0, row=8, sticky=tk.NW)
-ttk.Button(buttonFrame, text="ФИО и Группа местами", command=lambda: callFunc(z2_10SwapFIOasGroup, [data_dict])).grid(column=0, row=9, sticky=tk.NW)
+ttk.Button(buttonFrame, text="Уменьшить возраст (22)", command=lambda: callFunc(z2_6LimitBy22, [data_dict])).grid(
+    column=0, row=5, sticky=tk.NW)
+ttk.Button(buttonFrame, text="Удалить студента (23)", command=lambda: callFunc(z2_7DeleteBy23, [data_dict])).grid(
+    column=0, row=6, sticky=tk.NW)
+ttk.Button(buttonFrame, text="Увеличить возраст у Иванов",
+           command=lambda: callFunc(z2_8AddAgeByIvanov, [data_dict])).grid(column=0, row=7, sticky=tk.NW)
+ttk.Button(buttonFrame, text="Иванов на Сидоров", command=lambda: callFunc(z2_9ReplaceLNToSidorov, [data_dict])).grid(
+    column=0, row=8, sticky=tk.NW)
+ttk.Button(buttonFrame, text="ФИО и Группа местами", command=lambda: callFunc(z2_10SwapFIOasGroup, [data_dict])).grid(
+    column=0, row=9, sticky=tk.NW)
 
 root.mainloop()
